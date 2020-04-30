eb3250f0ab766f53b3e21c5c792b4afbd6df4c0a
QingQing Dou <qqdou@thoughtworks.com>
2020-04-30 12:02:56 +0800

    finish when number is 6, 6, 13, 15
    Signed-off-by: QingQing Dou <qqdou@thoughtworks.com>

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,9 +1,13 @@
 public class FizzBuzz {
     public String play(int num) {
-        if (num == 5) {
+        var numString = String.valueOf(num);
+        if (num % 5 == 0) {
             return "Buzz";
         }
-        if (num % 3 == 0) {
+        if (num == 7) {
+            return "Whizz";
+        }
+        if (numString.contains("3") || num % 3 == 0) {
             return "Fizz";
         }
         return String.valueOf(num);
--- test/fizzbuzz_specs.java
+++ test/fizzbuzz_specs.java
@@ -30,10 +30,46 @@ public class fizzbuzz_specs {
     }
 
     @Test
-    public void should_return_buzz_when_play_given_number_5() {
+    public void should_return_Buzz_when_play_given_number_5() {
 
         FizzBuzz fizzBuzz = new FizzBuzz();
         var result = fizzBuzz.play(5);
+
+        Assert.assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void should_return_Fizz_when_play_given_number_6() {
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        var result = fizzBuzz.play(6);
+
+        Assert.assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void should_return_Whizz_when_play_given_number_7() {
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        var result = fizzBuzz.play(7);
+
+        Assert.assertEquals("Whizz", result);
+    }
+
+    @Test
+    public void should_return_Fizz_when_play_given_number_13() {
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        var result = fizzBuzz.play(13);
+
+        Assert.assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void should_return_Buzz_when_play_given_number_15() {
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        var result = fizzBuzz.play(15);
 
         Assert.assertEquals("Buzz", result);
     }
