fa3b8c97787beea30caed6ecfb46a6ae628fbeab
QingQing Dou <qqdou@thoughtworks.com>
2020-04-30 12:12:28 +0800

    finish when number is 17, 21
    Signed-off-by: QingQing Dou <qqdou@thoughtworks.com>

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,15 +1,22 @@
 public class FizzBuzz {
     public String play(int num) {
         var numString = String.valueOf(num);
+        boolean isContain3 = numString.contains("3");
+        boolean isContain5 = numString.contains("5");
+        var result = "";
+        if (!isContain5 && num % 3 == 0) {
+            result += "Fizz";
+        }
         if (num % 5 == 0) {
-            return "Buzz";
+            result += "Buzz";
         }
-        if (num == 7) {
-            return "Whizz";
+        if (num % 7 == 0) {
+            result += "Whizz";
         }
-        if (numString.contains("3") || num % 3 == 0) {
+        if (isContain3) {
             return "Fizz";
         }
-        return String.valueOf(num);
+
+        return result.equals("") ? numString : result;
     }
 }
--- test/fizzbuzz_specs.java
+++ test/fizzbuzz_specs.java
@@ -73,4 +73,22 @@ public class fizzbuzz_specs {
 
         Assert.assertEquals("Buzz", result);
     }
+
+    @Test
+    public void should_return_17_when_play_given_number_17() {
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        var result = fizzBuzz.play(17);
+
+        Assert.assertEquals("17", result);
+    }
+
+    @Test
+    public void should_return_FizzWhizz_when_play_given_number_21() {
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        var result = fizzBuzz.play(21);
+
+        Assert.assertEquals("FizzWhizz", result);
+    }
 }
